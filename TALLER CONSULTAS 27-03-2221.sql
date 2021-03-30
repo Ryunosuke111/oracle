73461cda497e3420c01131dbdffdcc6582087979
Ryunosuke111 <jeyadrey16@gmail.com>
2021-03-29 20:08:09 -0500

    Signed-off-by: Ryunosuke111 <jeyadrey16@gmail.com>

--- TALLER CONSULTAS 27-03-2221.sql
+++ TALLER CONSULTAS 27-03-2221.sql
@@ -1,26 +1,44 @@
 
-
+# A
 
 SELECT * FROM  employees;
 
+# B
+
 SELECT * FORM employees ORDER BY first_name, last_name;
+
+# C
 
 SELECT * FORM employees WHERE first_name LIKE 'K%';
 
+# D
+
 SELECT * FROM employees WHERE first_name LIKE 'K%'  ORDER BY first_name; 
+
+# E
 
 SELECT department_id, COUNT(employee_id) FROM employees GROUP BY department_id;
 
+# F
+
 SELECT max(count(employee_id))as empleados FROM employees GROUP BY department_id ;
+
+# G
 
 SELECT employees.employee_id, employees.first_name, departments.department_name FROM employees 
 INNER JOIN departments ON departments.department_id = employees.employee_id;
 
+# H
+
 SELECT employees.employee_id, employees.first_name, employees.salary FROM employees 
 INNER JOIN departments ON departments.department_name = 'Sales';
 
+# I
+
 SELECT employees.employee_id, employees.first_name, employees.salary FROM employees 
 INNER JOIN departments ON departments.department_name = 'Sales' ORDER BY salary desc;
+
+
 
 /*Como los trabajos no tienen grado salarial asignado, agrego un campo a jobs
  definiendo salary_grade tipo number y mayor que 0*/
@@ -47,26 +65,66 @@ ALTER TABLE jobs ADD salary_grade NUMBER(2) CHECK (salary_grade>0);
  UPDATE jobs SET salary_grade=14 WHERE max_salary BETWEEN 65468 AND 79907;
  UPDATE jobs SET salary_grade=15 WHERE max_salary BETWEEN 79907 AND 96460;
 
+ # J
+
 SELECT employees.employee_id, employees.first_name, employees.salary, jobs.salary_grade FROM employees
 INNER JOIN jobs ON jobs.job_id = employees.job_id;
+
+# K
 
 SELECT employees.employee_id, employees.first_name, jobs.salary_grade FROM employees
 INNER JOIN jobs ON jobs.job_id = employees.job_id and (jobs.salary_grade = 2 or jobs.salary_grade = 4 or jobs.salary_grade = 5);
 
+# L
+
 SELECT department_id, avg(salary) FROM employees GROUP BY department_id ORDER BY avg(salary) desc;
+
+# M
+
+SELECT departments.department_name FROM departments 
+INNER JOIN (SELECT department_id as departamento FROM employees GROUP BY department_id ORDER BY avg(salary) desc) ON departments.department_id = departamento;
+
+# N
 
 SELECT department_id, COUNT(employee_id) FROM employees GROUP BY department_id 
 HAVING COUNT(employee_id) = (SELECT max(count(employee_id)) FROM employees GROUP BY department_id) ;
 
+# O
+
 SELECT employees.manager_id, employees.employee_id, employees.first_name, departments.department_name FROM employees
 INNER JOIN departments ON departments.manager_id = employees.manager_id;
+
+# P
+
+SELECT DISTINCT employees.employee_id, employees.first_name, jefe_grad,jobs.salary_grade, jefe_grad - jobs.salary_grade FROM employees 
+INNER JOIN jobs ON employees.job_id = jobs.job_id
+INNER JOIN (SELECT employees.employee_id as jefe_id, employees.department_id as jefe_dep, employees.manager_id as jefe_mana, jobs.salary_grade as jefe_grad 
+FROM employees 
+INNER JOIN jobs ON employees.job_id = jobs.job_id
+INNER JOIN departments ON departments.manager_id = employees.manager_id) ON employees.employee_id <> jefe_id
+WHERE employees.department_id = jefe_dep and employees.manager_id <> jefe_mana ORDER BY employees.employee_id asc;
+
+# Q
 
 SELECT DISTINCT departments.department_id, departments.department_name FROM departments 
 INNER JOIN employees ON employees.department_id = departments.department_id and employees.salary > 3000;
 
-SELECT DISTINCT d.department_id, d.department_name FROM departments d 
-INNER JOIN (SELECT department_id as departamento FROM employees GROUP BY department_id HAVING COUNT(employee_id)>2) ON d.department_id=departamento
-INNER JOIN employees e ON d.department_id=e.department_id WHERE e.salary>2500; 
+# R
+
+SELECT DISTINCT departments.department_id, departments.department_name FROM departments  
+INNER JOIN (SELECT department_id as departamento FROM employees GROUP BY department_id HAVING COUNT(employee_id)>2) ON departments.department_id=departamento
+INNER JOIN employees  ON departments.department_id=employees.department_id WHERE employees.salary>2500;
+
+# S
+
+SELECT DISTINCT employees.employee_id, employees.first_name FROM employees 
+INNER JOIN (SELECT employees.employee_id as jefe_id, employees.department_id as jefe_dep, employees.salary as jefe_salary, employees.manager_id as jefe_man FROM employees  
+INNER JOIN departments ON employees.manager_id = departments.manager_id) ON employees.employee_id <> jefe_id
+WHERE employees.department_id = jefe_dep AND employees.salary > jefe_salary AND employees.manager_id <>jefe_man ORDER BY employees.employee_id;
+
+
+
+
 
 
 
